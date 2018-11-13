888119fef622d622e5a792dd969111f05ad33420
Cameron Bacon <cameronmbacon@gmail.com>
2018-11-13 06:54:47 -0600

    created GET request to pokeAPI via HTTPS module, edited Pokemon ctor, added local env vars

--- Pokemon.js
+++ Pokemon.js
@@ -3,10 +3,22 @@ module.exports = function Pokemon(data) {
     this.name = data.name;
     this.height = data.height;
     this.weight = data.weight;
-    this.types = data.types;
-    this.species = data.species;
-    this.stats = data.stats;
-    this.moves = data.moves;
+    this.types = data.types.map(type => {
+        return {
+            slot: type.slot,
+            name: type.type.name,
+            url: type.type.url
+        }
+    });
+    this.species = data.species.url;
+    this.stats = data.stats.map(stat => {
+        return {
+            base_stat: stat.base_stat,
+            name: stat.stat.name,
+            url: stat.stat.url
+        }
+    });
+    this.moves = data.moves.map(move => move.move);
     this.defaultSprite = data.sprites.front_default;
 
     return this;
